fa098c1dadc2940e1f877285d2f894bf1fdb59f4
CrayZeeEeOne <ik2801730@gmail.com>
2026-07-30 19:53:07 +0300

    spi master

--- DF2/fsm_protocols/spi_master.sv
+++ DF2/fsm_protocols/spi_master.sv
@@ -1,66 +1,111 @@
 module spi_master #(
-  parameter N = 4 
+  parameter N = 4
 )(
   input logic clk, reset, start, miso,
   input logic [N-1:0] data,
   output logic mosi, done, sck, cs
 );
-  typedef enum logic [3:0] {
-    IDLE = 4'b0001,
-    RECORD = 4'b0010,
-    SHIFT = 4'b0100,
-    CHECK = 4'b1000 
+  typedef enum logic [1:0] { 
+    IDLE,
+    RECORD,
+    SHIFT,
+    CHECK
   } state_t;
 
   state_t current_state, next_state;
 
-  logic [N-1:0] shift_reg;
-  int bit_index;
+  logic [N-1:0] tx_shift_reg, tx_shift_reg_next;
+  logic [N-1:0] rx_shift_reg, rx_shift_reg_next;
+
+  logic [N-1:0] bit_index, bit_index_next;
+
+  logic mosi_next;
+
+  logic sck_prev;
 
   always_ff @(posedge clk, posedge reset) begin
-    if (reset) 
+    if (reset) begin
       current_state <= IDLE;
-    else
+      tx_shift_reg <= 0;
+      rx_shift_reg <= 0;
+      bit_index <= 0;
+      sck_prev <= 0;
+      mosi <= 0;
+    end
+    else begin
       current_state <= next_state;
+      tx_shift_reg <= tx_shift_reg_next;
+      rx_shift_reg <= rx_shift_reg_next;
+      bit_index <= bit_index_next;
+      sck_prev <= sck;
+      mosi <= mosi_next;
+    end
+  end
+  
+  logic [1:0] cnt;
+  // sck = 1/2 clk
+  always_ff @(posedge clk, posedge reset) begin
+    if (reset) begin
+      cnt <= 2'd0;
+      sck <= 0;
+    end
+    else if (cnt == 2'd1 & ~cs) begin
+      cnt <= 2'd0;
+      sck <= ~sck;
+    end
+    else if (~cs) begin
+      cnt <= cnt + 1'b1;
+      sck <= 0;
+    end
   end
 
   always_comb begin
     next_state = current_state;
 
+    tx_shift_reg_next = tx_shift_reg;
+    rx_shift_reg_next = rx_shift_reg;
+    bit_index_next = bit_index;
+    mosi_next = mosi;
 
-    case(current_state)
-      IDLE: begin
-        mosi = 0;
-        done = 0;
-        sck = 0;
-        cs = 1;
-        bit_index = 0;
-        shift_reg = data;
-        if (start) next_state = RECORD;
+    done = 0;
+    cs = 1;
+
+    case (current_state)
+      IDLE:
+        if (start) begin
+          cs = 0;
+          tx_shift_reg_next = data;
+          bit_index_next = N;
+          next_state = RECORD;
+        end
         else next_state = IDLE;
-      end
       RECORD: begin
-        bit_index = N;
-        next_state = SHIFT;
+        cs = 0;
+        if (~sck & sck_prev) begin //sck rising edge
+          rx_shift_reg_next[N-1] = miso;
+          mosi_next = tx_shift_reg[0];
+          bit_index_next--;
+          next_state = CHECK;
+        end
       end
       SHIFT: begin
         cs = 0;
-        sck = 1;
-        mosi = shift_reg[0];
-        shift_reg = shift_reg >> 1;
-        bit_index--;
-        next_state = CHECK;
+        if (sck & ~sck_prev) begin //sck falling edge
+          tx_shift_reg_next = tx_shift_reg >> 1;
+          rx_shift_reg_next = rx_shift_reg >> 1;
+          next_state = RECORD;
+        end
       end
       CHECK: begin
-        sck = 0;
         if (bit_index == 0) begin
           done = 1;
+          cs = 1;
           next_state = IDLE;
         end
         else begin
+          cs = 0;
           next_state = SHIFT;
         end
-    
       end
     endcase
   end
